f323276dbb40a20c56d0fd5b1f45611677524f12
mauricekoenig <60366053+mauricekoenig@users.noreply.github.com>
2021-08-19 14:15:54 +0200

    Alle Namen zur gleichen Zeit ausgeben!
    CHANGES:
    - Das Pushen der Namen erfolgt nun über eine ausgelagerte Funktion um Lesbarkeit zu steigern.
    - Das unorderedList-Element wird nun in einer Konstante gespeichert. Zuvor wurde das Element lokal innerhalb der Funktion angesprochen.
    - (optional) Die Methode "ausgabe" wurde zu print umbenannt - müsste also global übernommen werden.
    
    NEW:
    - Namen werden nun über einen simplen  Loop linear hinzugefügt. Die Funktion verbirgt sich hinter zwei "Guards", die gewährleisten, dass Elemente enthalten sind, oder zuvor noch keine Namen ausgegeben wurden.

--- tableReader.js
+++ tableReader.js
@@ -1,62 +1,50 @@
+
+// tableReader.js - Letze Änderung: Maurice König
+
 "use strict";
 
-/********  Variablen **********/
-
-// ein Objekt "person" wird angelegt
-const person = {
-    firstName: "Anton",
-    lastName: "Mustermann"
-}
-// Variable persons wird als Array angelegt
 let persons = [];
-
-
 const dispBtn1 = document.getElementById("dispBtn1");
-
-
-/******** Event Listener **********/
+const ul1 = document.getElementById("ul1"); 
 
 dispBtn1.addEventListener("click", showList);
+pushNames();
+print(persons);
 
-/******** business logic **********/
-
-
-// Das Objekt "person" wird in das Array "persons" gepusht
-persons.push(person);
-
-// Dem Array persons wird ein weiteres Objekt hinzugefügt, das zuvor nicht extra deklariert wurde
-persons.push({
-    firstName: "Moritz",
-    lastName: "Mustermann"
-});
-persons.push({
-    firstName: "Berta",
-    lastName: "Bertelsmann"
-});
-
-// Ausgaben zu Testzwecken in der Console
-// ausgabe(person);
-// ausgabe(person.firstName);
-ausgabe(persons);
-// ausgabe(persons[0].firstName);
-// ausgabe(persons[0].lastName);
-
-
-/************** Änderungen an HTML / Viewschicht *****************/
-
-
-// Zeigt das erste Element des Arrays "persons" in einer Liste im HTML Dokument an
 function showList() {
-    let newLi = document.createElement("li");
-    newLi.innerText = persons[0].firstName + " " + persons[0].lastName;
-    document.getElementById("ul1").appendChild(newLi);
+
+    if (ul1.childElementCount > 0) return;
+    if (persons.length == 0) return;
+
+    for (let i = 0; i < persons.length; i++){
+
+        let newListElement = document.createElement("li");
+        newListElement.innerText = persons[i].firstName + " " + persons[i].lastName;
+        ul1.appendChild(newListElement);
+    }
+}
+
+function pushNames (){
+
+    const person = {
+        firstName: "Anton",
+        lastName: "Mustermann"
+    }
+    
+    persons.push(person);
+    
+    persons.push({
+        firstName: "Moritz",
+        lastName: "Mustermann"
+    });
+    
+    persons.push({
+        firstName: "Berta",
+        lastName: "Bertelsmann"
+    });
 }
 
 
-
-
-
-/* Tools */
-function ausgabe(outputStr) {
+function print (outputStr) {
     console.log(outputStr);
 }
